919c4446a94a4de26db973335c8bbb9e6473acf5
andrastoth87 <andrewtoth87@gmail.com>
2022-12-15 13:16:46 -0500

    refactoring

--- processors/processor_template.py
+++ processors/processor_template.py
@@ -5,12 +5,14 @@ from abc import ABC, abstractmethod
 class ProcessorTemplate(ABC):
     """ A small template to use for the subtitle processors. """
 
-    def __init__(self, regex):
+    def __init__(self, regex) -> None:
         self._regex = regex
 
     @abstractmethod
     def shift(self, text, amount):
+        """ Main method to call to shift the subtitle. """
         raise NotImplementedError
 
     def _show_error(self, title, error_text):
+        """ Shows an error message. """
         tkMessagebox.showerror(title, error_text)
